96e144ee3a9088d3fc7401ac50f9bc20b8ec5fe6
MarcosMIzquierdoLinares <marcos.izquierdo.linares@gmail.com>
2025-03-21 18:48:53 +0100

    Realizada migración con éxito.

--- database/migrations/0001_01_01_000002_create_users_table.php
+++ database/migrations/0001_01_01_000002_create_users_table.php
@@ -6,19 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname'); 
+            $table->string('name_user')->unique(); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable(); 
             $table->string('password');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade'); 
+            $table->string('photo')->nullable(); 
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes(); 
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -40,8 +42,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
